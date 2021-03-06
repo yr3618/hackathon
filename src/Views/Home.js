0fd8b9a9e4e88cca54cd669d94e166d5c111badb
marta masramon <marta.masramon@gmail.com>
2021-03-06 16:04:22 +0000

    Images

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -5,20 +5,16 @@ function Home() {
   return (
     <div>
       <div className="home">
-      
-        <p>Donate money to the charity of your choice by recycling!</p>
-       
-      </div>
 
-      <div className="transition">
-        <img src={process.env.PUBLIC_URL + "/images/beigeToYellow.png"} alt="transition"></img>
+        <p>Donate money to the charity of your choice by recycling!</p>
+
       </div>
 
       <div className="home yellow">
         <h2>A subtitle</h2>
         <p>How does it work?</p>
         <p>Scan the products you want to recycle and once you have scanned 100 items,and we will donate £2 to the charity of your choice!</p>
-        
+
       </div>
     </div>
   );
