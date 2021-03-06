fc8ce19876054672104a0acfe80fadf11fb0251e
marta masramon <marta.masramon@gmail.com>
2021-03-06 16:21:57 +0000

    changed navbars

--- src/Navbars/Footer.js
+++ src/Navbars/Footer.js
@@ -6,12 +6,13 @@ function Footer() {
   return (
     <div className="footer">
       <Navbar expand="lg">
-        <Navbar.Brand href="#home">Seedling</Navbar.Brand>
+        <Navbar.Brand className="logo" href="#home">Seedling</Navbar.Brand>
         <Navbar.Collapse >
           <Nav className="ms-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/help">Help</Nav.Link>
-            <Nav.Link href="/whoarewe">Who Are We?</Nav.Link>
+            <Nav.Link href="/help">How to recycle</Nav.Link>
+            <Nav.Link href="/charities">Charities</Nav.Link>
+            <Nav.Link href="/contact">Who are we?</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- src/Navbars/Navbar.js
+++ src/Navbars/Navbar.js
@@ -6,14 +6,16 @@ function NavigationBar() {
   return (
     <div className="topnav">
       <Navbar expand="lg">
-        <Navbar.Brand href="#home">Seedling</Navbar.Brand>
+        <Navbar.Brand className="logo" href="#home">
+          <img className="logo-img" src={process.env.PUBLIC_URL + "/images/tree_sm.png"} alt="logo"></img>Seedling</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/help">Help</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Nav.Link href="/help">How to recycle</Nav.Link>
+            <Nav.Link href="/charities">Charities</Nav.Link>
+            <Nav.Link href="/contact">Who are we?</Nav.Link>
           </Nav>
 
         </Navbar.Collapse>
