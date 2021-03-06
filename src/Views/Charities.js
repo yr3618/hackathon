f28462c010dd244d9b406e6dffbf9220ed09498a
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 15:38:03 +0000

    Update Charities.js

--- src/Views/Charities.js
+++ src/Views/Charities.js
@@ -5,7 +5,9 @@ function Charities() {
   return (
     <div className="Charities">
       <h1>Charities we work with</h1>
-      <p>Paragraph</p>
+    
+    <h4>Macmillan Cancer Support is one of the largest British charities. It provides healthcare information and financial support to people affected by cancer.</h4>
+     
     </div>
   );
 }
