0996f64131a3e63e246a93f68c5501044d3a846b
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 16:00:36 +0000

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -5,8 +5,9 @@ function Home() {
   return (
     <div>
       <div className="home">
-        <h1>Home</h1>
-        <p>Paragraph</p>
+      
+        <p>Donate money to the charity of your choice by recycling!</p>
+       
       </div>
 
       <div className="transition">
@@ -15,9 +16,9 @@ function Home() {
 
       <div className="home yellow">
         <h2>A subtitle</h2>
-        <p>Donate money to the charity of your choice by donating!</p>
         <p>How does it work?</p>
         <p>Scan the products you want to recycle and once you have scanned 100 items,and we will donate £2 to the charity of your choice!</p>
+        
       </div>
     </div>
   );
