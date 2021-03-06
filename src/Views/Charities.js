2651a69c0d00120e1f7d14870ecfee2a4699f84a
yr3618 <64503502+yr3618@users.noreply.github.com>
2021-03-06 16:47:22 +0000

    Update Charities.js

--- src/Views/Charities.js
+++ src/Views/Charities.js
@@ -5,12 +5,17 @@ function Charities() {
   return (
     <div className="Charities">
     
-    <hr style="height:1px;border-top:1px solid #f00" />
+    <div class="charity 1">
+    <h2>British Heart Fundation</h2>
+    <p>British Heart Fundation is funds research related to heart and circulatory diseases.</p>
+    </div>
     
-    <p style="text-align:center;">Learn for free</p>
+    <div class="charity 2">
+    <h2>Macmillan Cancer support</h2>
+    <p>Macmillan cancer support is one of the largest british charities. It provides healthcare, information and financial support to people affected by cancer.</p>
+    </div>
     
-    <h6>Macmillan Cancer Support is one of the largest British charities. It provides healthcare information and financial support to people affected by cancer.</h6>
-     
+    
     </div>
   );
 }
