11b37d525f9ae61a865ccdc488ba3433035ca276
marta masramon <marta.masramon@gmail.com>
2021-03-06 15:50:34 +0000

    Home page changes

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,13 +4,18 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <div  className="home">
+      <div className="home">
         <h1>Home</h1>
         <p>Paragraph</p>
       </div>
 
-      <div  className="transition">
+      <div className="transition">
         <img src={process.env.PUBLIC_URL + "/images/beigeToYellow.png"} alt="transition"></img>
+      </div>
+
+      <div className="home yellow">
+        <h2>A subtitle</h2>
+        <p>Some jbdf jas fkjsdsjhdfg ajshglajhfg ajhgakjhgksjhg kjhg akjhg skhgklsjhfg kjhfg kdsjfhgljdhfg lkafghkahjsglkjfhglkfghgla lkjsf lakjflkd jflkajflkaj klfj alkjfl kdj lk jsdlkgj slkdj lksdjk lsdj flkeajf akj sjdgklj tiwoeisdf neuishgdvnjskd hievnm alk lea fael qeafj kl</p>
       </div>
     </div>
   );
